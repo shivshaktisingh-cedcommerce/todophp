6b37b3d56213f87b59abca1eb24108f4fd63b7eb
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-05-26 17:45:47 +0530

    add to cart using session

---
+++ src/config.php
@@ -0,0 +1,17 @@
+
+<?php
+$products = array (
+  array("ID"=>101, "name"=>"football", "image"=>"football.png",  
+  "price"=>150),
+  array("ID"=>102, "name"=>"tennis", "image"=>"tennis.png",  
+  "price"=>120),
+  array("ID"=>103, "name"=>"basketball", "image"=>"basketball.png",  
+  "price"=>90),
+  array("ID"=>104, "name"=>"table-tennis", "image"=>"table-tennis.png",  
+  "price"=>110),
+  array("ID"=>105, "name"=>"soccer", "image"=>"soccer.png",  
+  "price"=>80),
+  
+ 
+);
+?>
